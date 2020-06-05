2e59cc2f18fa4b68533c98154380fe2b2e59208f
Your Name <you@example.com>
2020-06-06 01:36:34 +0530

    toggle1

--- src/app/root-nav/root-nav.component.ts
+++ src/app/root-nav/root-nav.component.ts
@@ -9,15 +9,16 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./root-nav.component.css']
 })
 export class RootNavComponent {
-
+  opened = false;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
-    toggleMenu() {
-      this.toggleMenu();
-  }
+    toggleSidebar() {
+      this.opened = !this.opened;
+    }
+  
   constructor(private breakpointObserver: BreakpointObserver) {}
 
 }
