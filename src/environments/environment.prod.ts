2dbaae2d0910fda0f6c0fdab86e7f83c43a3e84d
Your Name <you@example.com>
2020-06-08 20:28:32 +0530

    firebaseoderinsertv1.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAsrd--ngSBdqVnFBGs4FSbqb2ml4bQ-5g",
+    authDomain: "milkpatna-389f6.firebaseapp.com",
+    databaseURL: "https://milkpatna-389f6.firebaseio.com",
+    projectId: "milkpatna-389f6",
+    storageBucket: "milkpatna-389f6.appspot.com",
+    messagingSenderId: "970143758066",
+    appId: "1:970143758066:web:990e07f3aedf7070b3426b",
+    measurementId: "G-61VN3ZBWJ2"
+  }
 };
